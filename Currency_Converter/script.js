e45054e2098ac82b98b9fb65aff576bbf7fc4937
Venkata Hanuma Teja . Kampa <hanumateja5050@gmail.com>
2023-12-17 21:51:08 +0530

    Add files via upload

---
+++ Currency_Converter/script.js
@@ -0,0 +1,231 @@
+
+let api =`https://v6.exchangerate-api.com/v6/7454ea7b62b55fd7e962b158/latest/USD`;
+
+const from = document.getElementById("from-currency");
+const to = document.getElementById("to-currency");
+
+
+currencies = [
+    "AED",
+    "AFN",
+    "ALL",
+    "AMD",
+    "ANG",
+    "AOA",
+    "ARS",
+    "AUD",
+    "AWG",
+    "AZN",
+    "BAM",
+    "BBD",
+    "BDT",
+    "BGN",
+    "BHD",
+    "BIF",
+    "BMD",
+    "BND",
+    "BOB",
+    "BRL",
+    "BSD",
+    "BTC",
+    "BTN",
+    "BWP",
+    "BYN",
+    "BZD",
+    "CAD",
+    "CDF",
+    "CHF",
+    "CLF",
+    "CLP",
+    "CNY",
+    "COP",
+    "CRC",
+    "CUC",
+    "CUP",
+    "CVE",
+    "CZK",
+    "DJF",
+    "DKK",
+    "DOP",
+    "DZD",
+    "EGP",
+    "ERN",
+    "ETB",
+    "EUR",
+    "FJD",
+    "FKP",
+    "GBP",
+    "GEL",
+    "GGP",
+    "GHS",
+    "GIP",
+    "GMD",
+    "GNF",
+    "GTQ",
+    "GYD",
+    "HKD",
+    "HNL",
+    "HRK",
+    "HTG",
+    "HUF",
+    "IDR",
+    "ILS",
+    "IMP",
+    "INR",
+    "IQD",
+    "IRR",
+    "ISK",
+    "JEP",
+    "JMD",
+    "JOD",
+    "JPY",
+    "KES",
+    "KGS",
+    "KHR",
+    "KMF",
+    "KPW",
+    "KRW",
+    "KWD",
+    "KYD",
+    "KZT",
+    "LAK",
+    "LBP",
+    "LKR",
+    "LRD",
+    "LSL",
+    "LYD",
+    "MAD",
+    "MDL",
+    "MGA",
+    "MKD",
+    "MMK",
+    "MNT",
+    "MOP",
+    "MRO",
+    "MRU",
+    "MUR",
+    "MVR",
+    "MWK",
+    "MXN",
+    "MYR",
+    "MZN",
+    "NAD",
+    "NGN",
+    "NIO",
+    "NOK",
+    "NPR",
+    "NZD",
+    "OMR",
+    "PAB",
+    "PEN",
+    "PGK",
+    "PHP",
+    "PKR",
+    "PLN",
+    "PYG",
+    "QAR",
+    "RON",
+    "RSD",
+    "RUB",
+    "RWF",
+    "SAR",
+    "SBD",
+    "SCR",
+    "SDG",
+    "SEK",
+    "SGD",
+    "SHP",
+    "SLL",
+    "SOS",
+    "SRD",
+    "SSP",
+    "STD",
+    "STN",
+    "SVC",
+    "SYP",
+    "SZL",
+    "THB",
+    "TJS",
+    "TMT",
+    "TND",
+    "TOP",
+    "TRY",
+    "TTD",
+    "TWD",
+    "TZS",
+    "UAH",
+    "UGX",
+    "USD",
+    "UYU",
+    "UZS",
+    "VEF",
+    "VES",
+    "VND",
+    "VUV",
+    "WST",
+    "XAF",
+    "XAG",
+    "XAU",
+    "XCD",
+    "XDR",
+    "XOF",
+    "XPD",
+    "XPF",
+    "XPT",
+    "YER",
+    "ZAR",
+    "ZMW",
+    "ZWL"
+];
+
+
+currencies.forEach(element => 
+{
+    const option = document.createElement("option");
+    option.value = element;
+    option.text = element;
+    from.add(option);   
+});
+
+currencies.forEach(element => 
+{
+    const option = document.createElement("option");
+    option.value = element;
+    option.text = element;
+    to.add(option);   
+});
+
+from.value = "USD";
+to.value = "INR";
+
+let convertCurrecny = () =>
+{
+    const amount = document.querySelector("#amount").value;
+    const fromCurrency = from.value;
+    const toCurrency = to.value;
+    
+    if(amount.length !=0)
+    {
+       fetch(api)
+       .then((resp) => resp.json())
+       .then((data) => 
+       {
+        let fromExchangeRate = data.conversion_rates[fromCurrency];
+        let toExchangeRate = data.conversion_rates[toCurrency];
+        const convertedAmount = (amount / fromExchangeRate) * toExchangeRate;
+        result.innerHTML = `${amount} ${fromCurrency} = ${convertedAmount.toFixed(2)} ${toCurrency}`;
+        error.textContent = "";
+       });
+    }
+    else
+    {
+        const error = document.querySelector("#error");
+        error.textContent = "Please Enter the Amount";
+        result.textContent = "";
+    } 
+};
+
+document
+    .querySelector("#convert")
+    .addEventListener("click", convertCurrecny);
+window.addEventListener("load", convertCurrecny);
